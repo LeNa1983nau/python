594bf8d63c9e722fa9b06f7039fcee42a16c8109
LeNa1983nau <lena.ai.itnau@gmail.com>
2026-09-03 21:55:37 +0700

    bai tap kiem tra so nguyen to

---
+++ duan1.py
@@ -0,0 +1,15 @@
+def la_so_nguyen_to(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+so = int(input("Nhập một số nguyên: "))
+
+if la_so_nguyen_to(so):
+    print(f"{so} là số nguyên tố.")
+else:
+    print(f"{so} không phải là số nguyên tố.")
